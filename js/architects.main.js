4b9103e4d45d1252a250c45bbe87820c307edb20
raid21 <raidski20@gmail.com>
2020-04-30 17:16:49 +0100

    1st commit

---
+++ js/architects.main.js
@@ -0,0 +1,14 @@
+$('.navbar-nav .nav-link').click(function () {
+    $('.navbar-nav .nav-link').removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.second-button').on('click', function () {
+    $('.animated-icon2').toggleClass('open');
+});
+
+$('.navbar-nav li a').click(function () {
+    $('html, body').animate({
+        scrollTop: $('#' + $(this).data('value')).offset().top
+    }, 1000);
+});
